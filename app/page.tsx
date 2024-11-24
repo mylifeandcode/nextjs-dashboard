f923e99a4104ac970ad8e387a7ae46948b27ab70
Alan Hummel <slade73@hotmail.com>
2024-11-24 13:08:03 -0500

    Correction for code from Chapter 3 related to mobile-version image

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+//import styles from '@/app/ui/home.module.css'; -- Non-Tailwind version
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      {/*<div className={styles.shape} /> Non-Tailwind Example*/}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
@@ -43,7 +45,7 @@ export default function Page() {
             width={560}
             height={620}
             className="block md:hidden"
-            alt="Screenshots of the dashboard project showing desktop version"
+            alt="Screenshots of the dashboard project showing mobile version"
           />          
         </div>
       </div>
